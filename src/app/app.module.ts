9ff59d9c06dd8d45fdad85585e4ac06ce245866d
fityocsaba96 <fcsabika@gmail.com>
2019-12-04 19:27:49 +0100

    feat(app): add initial angular project

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { RoutingModule } from './modules/routing/routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, RoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
