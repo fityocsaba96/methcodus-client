e1689fb2b19ef19d4353db7e4ffdb1b59a265d14
fityocsaba96 <fcsabika@gmail.com>
2019-12-12 23:51:23 +0100

    feat(env): make env vars available in prod mode

--- src/server.js
+++ src/server.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const compression = require('compression');
+const fs = require('fs');
+const cheerio = require('cheerio');
 
-const app = express();
+const getProcessWithEnvVars = require('../scripts/get-process-with-env-vars');
+
 const root = 'dist';
+const indexPath = '/index.html';
 
-app.use(compression());
-app.use(express.static(root));
-app.get('/*', (_, res) => res.sendFile('/', { root }));
-app.listen(process.env.PORT);
+const getHtmlWithEnvVars = () => {
+  const html = fs.readFileSync(root + indexPath, 'utf8');
+  const $ = cheerio.load(html);
+  const script = `<script>var process = ${JSON.stringify(getProcessWithEnvVars())};</script>`;
+  $('body').append(script);
+  return $.html();
+};
+
+const html = getHtmlWithEnvVars();
+const static = express.static(root);
+const server = express();
+
+server.use(compression());
+server.use((request, response, next) => {
+  if (request.path !== '/' && request.path !== indexPath) {
+    static(request, response, next);
+  } else {
+    next();
+  }
+});
+server.get('/*', (_, response) => response.send(html));
+server.listen(process.env.PORT);
