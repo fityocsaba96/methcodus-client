54626c22d6f005d2018830d372926c3bd6ccd9ef
fityocsaba96 <fcsabika@gmail.com>
2019-12-13 15:56:24 +0100

    feat(server): redirect all to https

--- src/server.js
+++ src/server.js
@@ -4,6 +4,14 @@ const fs = require('fs');
 const cheerio = require('cheerio');
 const getProcessWithEnvVars = require('../scripts/get-process-with-env-vars');
 
+const redirectToHttps = () => (request, response, next) => {
+  if (request.headers['x-forwarded-proto'] !== 'https') {
+    response.redirect(301, `https://${request.hostname}${request.url}`);
+  } else {
+    next();
+  }
+};
+
 const serveStaticWithoutIndex = (root, indexPath) => {
   const static = express.static(root);
   return (request, response, next) => {
@@ -28,6 +36,8 @@ const root = 'dist';
 const indexPath = '/index.html';
 const server = express();
 
+server.enable('trust proxy');
+server.use(redirectToHttps());
 server.use(compression());
 server.use(serveStaticWithoutIndex(root, indexPath));
 server.get('/*', serveIndexWithEnvVars(root + indexPath));
