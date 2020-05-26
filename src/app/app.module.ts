1b16320d1e3c7662f9e540aae415534c03e6a1c2
fityocsaba96 <fcsabika@gmail.com>
2020-05-27 00:02:30 +0200

    feat(style): add primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { RoutingModule } from './modules/routing/routing.module';
 import { AppComponent } from './app.component';
 import { CodeEditorComponent } from './components/code-editor/code-editor.component';
 
 @NgModule({
   declarations: [AppComponent, CodeEditorComponent],
-  imports: [BrowserModule, RoutingModule],
+  imports: [BrowserModule, BrowserAnimationsModule, RoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
