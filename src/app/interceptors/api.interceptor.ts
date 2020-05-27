8b6599cd3c0a431a13d14fcf62381bbdf67f0c0d
fityocsaba96 <fcsabika@gmail.com>
2020-05-27 11:51:37 +0200

    feat(api): include jwt token on api requests and logout on auth error

--- src/app/interceptors/api.interceptor.ts
+++ src/app/interceptors/api.interceptor.ts
@@ -1,10 +1,29 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Injectable()
 export class APIInterceptor implements HttpInterceptor {
+  constructor(private readonly router: Router, private readonly authService: AuthService) {}
+
   public intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(request.clone({ url: process.env.SERVER_URI + request.url }));
+    const requestUpdates: any = { url: process.env.SERVER_URI + request.url };
+    if (this.authService.isLoggedIn()) {
+      requestUpdates.setHeaders = { Authorization: 'Bearer ' + this.authService.jwtToken };
+    }
+    return next.handle(request.clone(requestUpdates)).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          this.authService.logout();
+          if (this.router.url !== '/login') {
+            this.router.navigateByUrl('/login');
+          }
+        }
+        return throwError(error);
+      }),
+    );
   }
 }
