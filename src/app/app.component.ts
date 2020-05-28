06df57affc2f9df91ec888ece7dc22c2eac0b52b
fityocsaba96 <fcsabika@gmail.com>
2020-05-28 19:29:35 +0200

    feat(menu): show active pair programming request on menubar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { MenuItem, MessageService } from 'primeng/api';
 import { AuthService } from './services/auth.service';
 import { Router } from '@angular/router';
+import { PairProgrammingRequestService } from './services/pair-programming-request.service';
+import { GetMyPairProgrammingRequestResponse } from './interfaces/responses/pair-programming-request.response';
+import { values } from 'ramda';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +13,10 @@ import { Router } from '@angular/router';
   providers: [MessageService],
 })
 export class AppComponent implements OnInit {
-  public menuItems: MenuItem[] = [
-    {
+  private menu: { [name: string]: MenuItem } = {
+    pairProgrammingRequest: { styleClass: 'pair-programming-request', visible: false },
+    separator: { separator: true, visible: false },
+    myAccount: {
       label: 'My account',
       items: [
         { label: 'My solutions', routerLink: 'user/solutions' },
@@ -21,15 +26,57 @@ export class AppComponent implements OnInit {
         { label: 'Logout', command: this.onLogoutClick.bind(this) },
       ],
     },
-  ];
+  };
+
+  public menuItems: MenuItem[] = values(this.menu);
 
   public loggedIn: boolean;
 
-  constructor(private readonly router: Router, private readonly authService: AuthService) {}
+  constructor(
+    private readonly router: Router,
+    private readonly authService: AuthService,
+    private readonly pairProgrammingRequestService: PairProgrammingRequestService,
+  ) {}
 
   public ngOnInit(): void {
     this.loggedIn = this.authService.isLoggedIn();
     this.authService.loggedInChange.subscribe(loggedIn => (this.loggedIn = loggedIn));
+    this.checkPairProgrammingRequest();
+  }
+
+  public checkPairProgrammingRequest(): void {
+    this.pairProgrammingRequestService.getMine().subscribe(this.handlePairProgrammingRequest.bind(this));
+  }
+
+  private handlePairProgrammingRequest(response: GetMyPairProgrammingRequestResponse | void): void {
+    if (response !== null) {
+      this.showPairProgrammingRequest(response as GetMyPairProgrammingRequestResponse);
+    } else {
+      this.hidePairProgrammingRequest();
+    }
+  }
+
+  private showPairProgrammingRequest(response: GetMyPairProgrammingRequestResponse): void {
+    this.menu.pairProgrammingRequest.label = this.generatePairProgrammingRequestLabel(response);
+    this.menu.pairProgrammingRequest.routerLink = `exercises/${response.exerciseId}/solve`;
+    this.menu.pairProgrammingRequest.queryParams = {
+      language: response.programmingLanguage,
+      method: response.softwareDevelopmentMethod,
+      pair: response.pairUser._id,
+    };
+    this.menu.pairProgrammingRequest.visible = true;
+    this.menu.separator.visible = true;
+  }
+
+  private hidePairProgrammingRequest(): void {
+    this.menu.pairProgrammingRequest.visible = false;
+    this.menu.separator.visible = false;
+  }
+
+  private generatePairProgrammingRequestLabel(response: GetMyPairProgrammingRequestResponse): string {
+    const language = { javascript: 'JavaScript', java: 'Java' }[response.programmingLanguage];
+    const method = { 'pair-programming': 'pair programming', 'ping-pong': 'ping pong' }[response.softwareDevelopmentMethod];
+    return `${response.pairUser.userName} sent you a programming request! (${method} with ${language})`;
   }
 
   private onLogoutClick(): void {
